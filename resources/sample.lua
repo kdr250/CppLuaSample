fef17891c6c3f5e51e32e59a0d0eb090b3f5aff0
kdr250 <54400975+kdr250@users.noreply.github.com>
2025-05-29 15:13:54 +0900

    2. C++からLuaのグローバル変数を呼び出す

--- resources/sample.lua
+++ resources/sample.lua
@@ -2,4 +2,3 @@ test1 = 100;
 test2 = 200;
 test3 = test1 + test2;
 test4 = "AIUEO";
-test5 = test 1 + test 2;
--- src/Main.cpp
+++ src/Main.cpp
@@ -11,6 +11,23 @@ int main() {
         return EXIT_FAILURE;
     }
 
+    std::cout << "Sample: variable" << std::endl;
+    {
+        lua_getglobal(pL, "test1");
+        lua_getglobal(pL, "test3");
+        lua_getglobal(pL, "test2");
+        lua_getglobal(pL, "test4");
+
+        const int num = lua_gettop(pL);
+        for (int i = 1; i <= num; ++i) {
+            const int type = lua_type(pL, i);
+            std::cout << "type=" << type << ", name=" << lua_typename(pL, type)
+                      << ", val=" << lua_tonumber(pL, i) << ", str=" << lua_tostring(pL, i)
+                      << std::endl;
+        }
+        lua_pop(pL, num);
+    }
+
     lua_close(pL);
     return EXIT_SUCCESS;
 }
