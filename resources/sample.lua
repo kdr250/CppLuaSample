69e3f412e856e9695e15d8ea9b80981c986a03e8
kdr250 <54400975+kdr250@users.noreply.github.com>
2025-05-29 15:06:29 +0900

    1. Luaファイル読み込み

---
+++ resources/sample.lua
@@ -0,0 +1,5 @@
+test1 = 100;
+test2 = 200;
+test3 = test1 + test2;
+test4 = "AIUEO";
+test5 = test 1 + test 2;
--- src/Main.cpp
+++ src/Main.cpp
@@ -1,8 +1,16 @@
+#include <iostream>
 #include <lua.hpp>
 
 int main() {
     lua_State* pL = luaL_newstate();
 
+    if (luaL_dofile(pL, "resources/sample.lua") != LUA_OK) {
+        // Error
+        std::cerr << lua_tostring(pL, lua_gettop(pL)) << std::endl;
+        lua_close(pL);
+        return EXIT_FAILURE;
+    }
+
     lua_close(pL);
-    return 0;
+    return EXIT_SUCCESS;
 }
