768f7c7b823e758ae3fa0e6804eb6909764c874a
kdr250 <54400975+kdr250@users.noreply.github.com>
2025-05-29 15:27:57 +0900

    3. C++からLuaの関数を呼び出す

--- resources/sample.lua
+++ resources/sample.lua
@@ -1,4 +1,12 @@
-test1 = 100;
-test2 = 200;
-test3 = test1 + test2;
-test4 = "AIUEO";
+function fibonacci(val)
+    if val <= 0 then
+        return 0;
+    elseif val == 1 then
+        return 1;
+    end
+    return fibonacci(val - 1) + fibonacci(val - 2)
+end
+
+function multi(val1, val2)
+    return val1 + val2, val1 * val2, "aiueo"
+end
--- src/Main.cpp
+++ src/Main.cpp
@@ -11,21 +11,41 @@ int main() {
         return EXIT_FAILURE;
     }
 
-    std::cout << "Sample: variable" << std::endl;
+    std::cout << "Sample: function C++ call Lua" << std::endl;
     {
-        lua_getglobal(pL, "test1");
-        lua_getglobal(pL, "test3");
-        lua_getglobal(pL, "test2");
-        lua_getglobal(pL, "test4");
-
-        const int num = lua_gettop(pL);
-        for (int i = 1; i <= num; ++i) {
-            const int type = lua_type(pL, i);
-            std::cout << "type=" << type << ", name=" << lua_typename(pL, type)
-                      << ", val=" << lua_tonumber(pL, i) << ", str=" << lua_tostring(pL, i)
-                      << std::endl;
+        // 0: Calculate Fibonacci
+        const int nargs0 = 1;
+        const int nresults0 = 1;
+        const int msgh0 = 0;
+        lua_getglobal(pL, "fibonacci");
+        lua_pushnumber(pL, 10);
+        if (lua_pcall(pL, nargs0, nresults0, msgh0) != LUA_OK) {
+            std::cerr << lua_tostring(pL, lua_gettop(pL)) << std::endl;
+            lua_close(pL);
+            return EXIT_FAILURE;
         }
-        lua_pop(pL, num);
+        std::cout << "\t" << "fibonacci=" << lua_tointeger(pL, 1) << std::endl;
+
+        const int num1 = lua_gettop(pL);
+        lua_pop(pL, num1);
+
+        // 1: calculate multiple values
+        const int nargs1 = 2;
+        const int nresults1 = 3;
+        const int msgh1 = 0;
+        lua_getglobal(pL, "multi");
+        lua_pushnumber(pL, 3);
+        lua_pushnumber(pL, 7);
+        if (lua_pcall(pL, nargs1, nresults1, msgh1) != LUA_OK) {
+            std::cerr << lua_tostring(pL, lua_gettop(pL)) << std::endl;
+            lua_close(pL);
+            return EXIT_FAILURE;
+        }
+        std::cout << "\t" << "return=" << lua_tointeger(pL, 1) << ", " << lua_tointeger(pL, 2)
+                  << ", " << lua_tostring(pL, 3) << std::endl;
+
+        const int num2 = lua_gettop(pL);
+        lua_pop(pL, num2);
     }
 
     lua_close(pL);
